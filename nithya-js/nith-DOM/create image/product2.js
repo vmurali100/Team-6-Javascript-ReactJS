65b42cadb90b26dc5ce3fc8922290b9ed6447794
kollathurnithya <kollathurnithya7@gmail.com>
2022-05-10 10:57:52 +0530

    image

---
+++ nithya-js/nith-DOM/create image/product2.js
@@ -0,0 +1,23 @@
+
+ 
+   var employee= [
+      {
+        id: "1",
+        firstName: "Tom",
+        lastName: "Cruise",
+        image: "https://jsonformatter.org/img/tom-cruise.jpg"
+      },
+      {
+        id: "2",
+        firstName: "Maria",
+        lastName: "Sharapova",
+        image: "https://jsonformatter.org/img/Maria-Sharapova.jpg"
+      },
+      {
+        id: "3",
+        firstName: "Robert",
+        lastName: "Downey Jr.",
+        image: "https://jsonformatter.org/img/Robert-Downey-Jr.jpg"
+      }
+    ]
+  

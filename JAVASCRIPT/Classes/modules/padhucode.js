eeacd763d04817277748f433683c23c842d02f0e
Padmajamanju <mpadhupadmaja@gmail.com>
2022-06-07 16:20:42 +0530

    js

---
+++ JAVASCRIPT/Classes/modules/padhucode.js
@@ -0,0 +1,7 @@
+function sayhellotoallstudents(students){
+    students.forEach((std)=>{
+        console.log("hello "+std+" how r u")
+    })
+}
+var students=["seetha","ram","krishna","radha"]
+sayhellotoallstudents(students)

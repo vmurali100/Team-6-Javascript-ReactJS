a7800a6fb06d966791d6c91368a8f260245a51bd
deepthiboppana <harideepthiboppana@gmail.com>
2022-06-28 14:41:44 +0530

    form2.jsx

--- React/project8-async/src/App.js
+++ React/project8-async/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { Products } from './component/Products';
 import { Form1 } from './forms/Form1';
+import { Form2 } from './forms/Form2';
 
 function App() {
   return (
     <div className="App">
     {/* <Products/> */}
-    <Form1/>
+    {/* <Form1/> */}
+    <Form2/>
     </div>
   );
 }

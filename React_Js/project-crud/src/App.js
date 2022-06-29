df5b75ee5717803ecc3d74bfa8a29fa5f5d48462
Padmajamanju <mpadhupadmaja@gmail.com>
2022-06-29 13:48:34 +0530

    crud

--- React_Js/project-crud/src/App.js
+++ React_Js/project-crud/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import User from './Functional components/User';
+// import User1 from './Functional components/User1';
 
 function App() {
   return (
     <div className="App">
       <User/>
+      {/* <User1/> */}
     </div>
   );
 }
---
+++ React_Js/project-crud/src/Functional components/User1.jsx
@@ -0,0 +1,101 @@
+import React from 'react'
+import { useState } from 'react'
+import axios from 'axios'
+import { useEffect } from 'react'
+
+const User1 = () => {
+    const [user, setuser] = useState({fname: "", lname: "", dob: "", email: "", mobile: "",id:""})
+    const [allusers,setallusers]=useState([])
+    const [isedit,setisedit]=useState(false)
+    const handlechange = (event) => {
+        var inputuser = event.target.name
+        const newuser = { ...user }
+        newuser[inputuser] = event.target.value
+        setuser(newuser)
+    }
+    const getallusers=()=>{
+        axios.get("  http://localhost:3000/users1").then((response)=>{
+            setallusers(response.data)
+        })
+    }
+    const clearusers=()=>{
+        setuser({fname: "", lname: "", dob: "", email: "", mobile: ""})
+    }
+    const adduser = () => {
+        console.log(user)
+        axios.post("  http://localhost:3000/users1", user).then((response) => {
+            console.log("User added successfully")
+            clearusers()
+            getallusers()
+        })
+
+    }
+    useEffect(()=>{
+        getallusers()
+    },[])
+    const deleteuser=(userdetails)=>{
+        axios.delete("   http://localhost:3000/users1/"+ userdetails.id).then((response)=>{
+            getallusers()
+        })
+    }
+    const edituser=(userinfo)=>{
+        setisedit(true)
+        setuser(userinfo)
+    }
+    const updateuser=()=>{
+        axios.put(" http://localhost:3000/users1/"+user.id,user).then(()=>{
+            getallusers()
+            clearusers()
+            setisedit(false)
+        })
+    }
+    return (
+        <div>
+            <form>
+                <label htmlFor="">First Name : </label>
+                <input type="text" name='fname' value={user.fname} onChange={(event) => { handlechange(event) }} /><br />
+                <label htmlFor="">Last Name : </label>
+                <input type="text" name='lname' value={user.lname} onChange={(event) => { handlechange(event) }} /><br />
+                <label htmlFor="">Date Of Birth : </label>
+                <input type="text" name='dob' placeholder='dd-mm-yyyy' value={user.dob} onChange={(event) => { handlechange(event) }} /><br />
+                <label htmlFor="">Email ID : </label>
+                <input type="text" name='email' value={user.email} onChange={(event) => { handlechange(event) }} /><br />
+                <label htmlFor="">Mobile Number : </label>
+                <input type="text" name='mobile' value={user.mobile} onChange={(event) => { handlechange(event) }} /><br />
+                <input type="text" name="id" hidden />
+                {isedit ? <button onClick={updateuser} type="button">update user</button>: <button onClick={adduser} type="button">add user</button>}
+               
+                
+            </form>
+            <hr />
+            <table border={1}>
+                <thead>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Date Of Birth</th>
+                        <th>Email</th>
+                        <th>Mobile Number</th>
+                        <th>Edit User</th>
+                        <th>Delete User</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {allusers.map((userinfo,i)=>{
+                        return <tr key={i}>
+                            <td>{userinfo.fname}</td>
+                            <td>{userinfo.lname}</td>
+                            <td>{userinfo.dob}</td>
+                            <td>{userinfo.email}</td>
+                            <td>{userinfo.mobile}</td>
+                            <td><button onClick={()=>{edituser(userinfo)}}>Edit User</button></td>
+                            <td><button onClick={()=>{deleteuser(userinfo)}}>Delete User</button></td>
+                        </tr>
+                    })}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default User1

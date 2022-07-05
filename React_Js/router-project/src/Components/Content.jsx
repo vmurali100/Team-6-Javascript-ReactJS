56b996f5af1516db9f2a31f1ab0ce7be64b0d0de
Padmajamanju <mpadhupadmaja@gmail.com>
2022-07-05 14:24:00 +0530

    router1

---
+++ React_Js/router-project/src/Components/Content.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const Content = () => {
+    return (
+        <div>
+            <div class="wrapper row2">
+                <div id="container" class="clear">
+
+                    <section id="shout">
+                        <p>Vestibulumaccumsan egestibulum eu justo convallis augue estas aenean elit intesque sed. Facilispede estibulum nulla orna nisl velit elit ac aliquat non tincidunt. Namjusto cras urna urnaretra lor urna neque sed quis orci nulla laoremut vitae doloreet condimentumst.</p>
+                    </section>
+
+                    <div id="homepage">
+
+                        <section id="latest">
+                            <article>
+                                <figure>
+                                    <ul>
+                                        <li class="one_quarter"><img src="images/demo/215x150.gif" width="215" height="150" alt="" /></li>
+                                        <li class="one_quarter"><img src="images/demo/215x150.gif" width="215" height="150" alt="" /></li>
+                                        <li class="one_quarter"><img src="images/demo/215x150.gif" width="215" height="150" alt="" /></li>
+                                        <li class="one_quarter lastbox"><img src="images/demo/215x150.gif" width="215" height="150" alt="" /></li>
+                                    </ul>
+                                    <figcaption><a href="#">View All Our Recent Work Here &raquo;</a></figcaption>
+                                </figure>
+                            </article>
+                        </section>
+
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Content

d816cc4825f3bb2f0c9e87302758478d634a743e
srividya-T <vidyachowdary7@gmail.com>
2022-07-05 14:49:50 +0530

    basic88

--- React-Js/router-project/src/Components/Gallery.jsx
+++ React-Js/router-project/src/Components/Gallery.jsx
@@ -4,7 +4,9 @@ export const Gallery = () => {
   return (
     <div>
         <h2>Welcome to Gallery Component</h2>
-        <img src="images/demo/Flower1.jpg" width="450" height="250" alt=""/>
+        <img src="images/demo/Flower1.jpg" width="290" height="180" alt=""/>
+        <img src="images/demo/Flower2.jpg" width="290" height="180" alt=""/>
+        <img src="images/demo/Flower3.jpg" width="290" height="180" alt=""/>
     </div>
   )
 }

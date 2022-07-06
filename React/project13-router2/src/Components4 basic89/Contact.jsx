6bf658783fee53e057bd93dc979242c1df9b5170
deepthiboppana <harideepthiboppana@gmail.com>
2022-07-06 13:41:43 +0530

    router5

--- React/project13-router2/src/Components4 basic89/Contact.jsx
+++ React/project13-router2/src/Components4 basic89/Contact.jsx
@@ -3,49 +3,29 @@ import React from 'react'
 const Contact = () => {
   return (
     <div>
-        <div id="homepage">
+  <label for="fName">First Name : </label>
+        <input type="text" /> <br/>
+        <label for="lName">Last Name :</label>
+        <input type="text" name="" id="" step="3"/> <br/>
+        <label for="number">Mobile Number</label>
+        <input type="number" /><br/>
+        <label for="email">Email : </label>
+        <input type="password"/><br/>
+        <label for="dob">Date of birth : </label>
+        <input type="date"/><br/>
 
-<section id="services" class="clear">
-  <article class="one_quarter">
-    <figure><img src="images4/32x32.gif" width="32px" height="32px" alt=""/></figure>
-    <strong>Lorum ipsum dolor</strong>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non diam erat. In fringilla massa ut nisi ullamcorper.</p>
-    <p class="more"><a href="#">Read More &raquo;</a></p>
-  </article>
-  <article class="one_quarter">
-  <figure><img src="images4/32x32.gif" width="32px" height="32px" alt=""/></figure>
-    <strong>Lorum ipsum dolor</strong>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non diam erat. In fringilla massa ut nisi ullamcorper.</p>
-    <p class="more"><a href="#">Read More &raquo;</a></p>
-  </article>
-  <article class="one_quarter">
-  <figure><img src="images4/32x32.gif" width="32px" height="32px" alt=""/></figure>
-    <strong>Lorum ipsum dolor</strong>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non diam erat. In fringilla massa ut nisi ullamcorper.</p>
-    <p class="more"><a href="#">Read More &raquo;</a></p>
-  </article>
-  <article class="one_quarter lastbox">
-  <figure><img src="images4/32x32.gif" width="32" height="32" alt=""/></figure>
-    <strong>Lorum ipsum dolor</strong>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non diam erat. In fringilla massa ut nisi ullamcorper.</p>
-    <p class="more"><a href="#">Read More &raquo;</a></p>
-  </article>
-</section>
+        <select name="" id="">
+            <option value="">Html</option>
+            <option value="">Css</option>
+            <option value="">Js</option>
+            <option value="">react js</option>
+            <option value="">angular</option>
+        </select><br/>
 
-<section id="latest">
-  <article>
-    <figure>
-      <ul class="clear">
-        <li class="one_third"><img src="images4/290x180.gif" width="290" height="180" alt=""/></li>
-        <li class="one_third"><img src="images4/290x180.gif" width="290" height="180" alt=""/></li>
-        <li class="one_third lastbox"><img src="images4/290x180.gif" width="290" height="180" alt=""/></li>
-      </ul>
-      <figcaption><a href="#">View All Our Recent Work Here &raquo;</a></figcaption>
-    </figure>
-  </article>
-</section>
-
-</div>
+        <button>Submit</button><br/>
+        <textarea name="" id="" cols="30" rows="10"></textarea><br/>
+        <label for="pincode">Pincode</label>
+        <input type="number" value="pincode" Autofocus/>
 
     </div>
   )

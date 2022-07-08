9583b092906cf8de284b8acd9da9c2079f82dff6
deepthiboppana <harideepthiboppana@gmail.com>
2022-07-08 16:21:59 +0530

    redux3

--- React/project14-redux/src/store2/Store2.jsx
+++ React/project14-redux/src/store2/Store2.jsx
@@ -1,9 +1,9 @@
 const { createStore } = require("redux")
-const reducerFn = (state={count:0},action) => { 
-    switch(action.type){
+const reducerFn = (state = { count: 0 }, action) => {
+    switch (action.type) {
         case 'INCREMENT':
-            return{...state,count:state.count+1}
-            case "DECREMENT ":
+            return { ...state, count: state.count + 1 }
+            case 'DECREMENT':
                 return{...state,count:state.count-1}
     }
     return state

78e9d6dbe76b05222c86c4ff949a65b248766cfb
Padmajamanju <mpadhupadmaja@gmail.com>
2022-07-09 17:38:11 +0530

    reduxtool3,4

--- React_Js/redux-tool/src/index.js
+++ React_Js/redux-tool/src/index.js
@@ -7,21 +7,35 @@ import { Provider } from 'react-redux';
 import store from './store1/index1';
 import store2 from './store2/index2';
 import App2 from './store2/App2';
-
-
+import App3 from './store3/App3';
+import store3 from './store3/index3';
+import App4 from './store4/App4';
+import store4 from './store4/index4';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  </Provider>
+  // <Provider store={store}>
+  //   <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+  // </Provider>
 
   // <Provider store={store2}>
   //   <React.StrictMode>
   //     <App2/>
   //   </React.StrictMode>
   // </Provider>
+
+  // <Provider store={store3}>
+  //    <React.StrictMode>
+  //      <App3/>
+  //    </React.StrictMode>
+  // </Provider>
+
+  <Provider store={store4}>
+    <React.StrictMode>
+       <App4/>
+     </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ React_Js/redux-tool/src/store3/index3.js
@@ -0,0 +1,23 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+const counterSlice=createSlice({
+    name:"counter",
+    initialState:{counter:0},
+    reducers:{
+        incrementcount:(state)=>{
+            state.counter+=1
+        },
+        decrementcount:(state)=>{
+            state.counter-=1
+        },
+        addby:(state,action)=>{
+            state.counter+=action.payload
+        }
+    }
+})
+export const actions=counterSlice.actions
+const store3=configureStore({
+    reducer:counterSlice.reducer
+})
+export default store3;
+   
+

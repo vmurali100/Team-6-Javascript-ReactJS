a02f95ae3210fe514583e8faf30261f1cd0c018b
AKALYASRIJAK <akalyaksreejak@gmail.com>
2022-07-17 07:23:15 +0530

    code tqg

---
+++ React-JS/redux-toolkit-project3/src/Components/UserDetails.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useDispatch,useSelector } from 'react-redux';
+import { deleteUser,editUser } from '../slices/userSlice';
+
+export const UserDetails =()=> {
+    const allUsers=useSelector((state)=>state.userInfo.users)
+    const dispatch=useDispatch()
+
+     const handleDeleteuser=(i)=>{
+        dispatch(deleteUser(i))
+     }
+
+     const handleEdituser=(i)=>{
+        var info={
+            isEdit:true,
+            index:i
+        }
+        dispatch(editUser(info))
+     }
+  return (
+    <div>
+        <table border={1}>
+            <thead>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Edit</th>
+                <th>Delete</th> 
+            </thead>
+        </table>
+        <tbody>
+            {allUsers.map((user,i)=>{
+                return <tr key={i}>
+                    <td>{user.fname}</td>
+                    <td>{user.lname}</td>
+                    <td><button onClick={()=>{handleEdituser(i)}}>Edit</button></td>
+                    <td><button onClick={()=>{handleDeleteuser(i)}}>Delete</button></td>
+                </tr>
+            })}
+        </tbody>
+    </div>
+  )
+}
+export default UserDetails
